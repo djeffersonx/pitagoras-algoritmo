779a96e475c5445e4f28f555425e7555b70dbc46
djeffersonx <djeffersonx@gmail.com>
2020-12-23 11:56:32 -0300

    Update MainClass.java
    Fix input

--- src/br/com/djefferson/v1/MainClass.java
+++ src/br/com/djefferson/v1/MainClass.java
@@ -4,7 +4,7 @@ public class MainClass {
 
 	public static void main(String[] args) {
 
-		MinMaxSumAlgorithm minMaxSumAlgorithm = new MinMaxSumAlgorithm(new Integer[] { 5, -3, 7, 8, 2 });
+		MinMaxSumAlgorithm minMaxSumAlgorithm = new MinMaxSumAlgorithm(new Integer[] { 4, -3, 7, 8, 2 });
 
 		System.out.println(minMaxSumAlgorithm.getMinSum());
 		System.out.println(minMaxSumAlgorithm.getMaxSum());
